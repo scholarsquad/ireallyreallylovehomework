581e3141147f82bdb9a9582bb14ab6330ee952a6
scholarsquad <scholarsquadservice@gmail.com>
2025-05-08 11:23:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16862,8 +16862,7 @@ window.onload=function()
 				LoadLang('loc/'+lang+'.js?v='+Game.version,function(){
 					var launch=function(){
 						Game.Launch();
-						if true 
-							Game.ErrorFrame();
+						if (top!=self) Game.ErrorFrame();
 						else
 						{
 							console.log('[=== '+choose([
